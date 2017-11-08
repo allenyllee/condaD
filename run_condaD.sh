59cb97615df58010e876c7128d57f126f7971d02
allenyllee <allen7575@gmail.com>
2017-11-08 19:20:53 +0800

    initail commit

---
+++ run_condaD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+PSWD=$1
+
+docker run -ti \
+    --name anaconda \
+    --publish 8889:8888 \
+    --env DISPLAY=$DISPLAY \
+    --env XAUTHORITY=$XAUTH \
+    --env PASSWORD=$PSWD \
+    --volume $XSOCK:$XSOCK \
+    --volume $XAUTH:$XAUTH \
+    --volume $PWD/project/jupyter/notebooks:/opt/notebooks \
+    allenyllee/anaconda:latest
