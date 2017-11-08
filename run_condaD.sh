f2b5deb3278cf88796e085f477936fb859d69af1
allenyllee <allen7575@gmail.com>
2017-11-08 22:19:54 +0800

    set .docker.xauth everytime login

--- run_condaD.sh
+++ run_condaD.sh
@@ -3,6 +3,18 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
+
+# set .docker.xauth after login, becasue /tmp will be deleted everytime system startup
+# filesystem - How is the /tmp directory cleaned up? - Ask Ubuntu 
+# https://askubuntu.com/questions/20783/how-is-the-tmp-directory-cleaned-up
+# 
+tr '\n' '\000' < ~/.profile | sudo tee ~/.profile >/dev/null
+sed -i 's|\x00XAUTH=.*-\x00|\x00|' ~/.profile
+tr '\000' '\n' < ~/.profile | sudo tee ~/.profile >/dev/null
+echo "XAUTH=/tmp/.docker.xauth; touch $XAUTH; xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -" >> ~/.profile
+source ~/.profile
+
+
 PSWD=$1
 NOTEBOOK_DIR=$2
 
